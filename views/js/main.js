a3573f9bacd70c49e896d393504c08f3cac41569
Dru Martin <benjaminmartin6683@gmail.com>
2016-05-25 20:15:47 -0500

    update w/solution
    updated code removing unneeded functions and changing pizza sizes with % widths instead of calling for pixels and then making them a %. Also took all queryselectorall and made into a variable for the rule: do not repeat yourself

--- views/js/main.js
+++ views/js/main.js
@@ -419,45 +419,29 @@ var resizePizzas = function(size) {
     }
   }
 
-  changeSliderLabel(size);
-
-  // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
-  function determineDx (elem, size) {
-    var oldwidth = elem.offsetWidth;//read layout
-    var windowwidth = document.querySelector("#randomPizzas").offsetWidth;//read layout
-    var oldsize = oldwidth / windowwidth;
-    console.log(oldwidth);
-    console.log(windowwidth);
-    // Changes the slider value to a percent width
-    function sizeSwitcher (size) {
-      switch(size) {
-        case "1":
-          return 0.25;
-        case "2":
-          return 0.3333;
-        case "3":
-          return 0.5;
-        default:
-          console.log("bug in sizeSwitcher");
-      }
-    }
-
-    var newsize = sizeSwitcher(size);
-    var dx = (newsize - oldsize) * windowwidth;
-
-    return dx;
-  }
-
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);//read layout
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';//read layout
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;//write css
-      console.log(dx);
-      console.log(newwidth);
+    switch(size) {
+        case "1":
+          newWidth = 25;
+          break;
+        case "2":
+          newWidth = 33.3;
+          break;
+        case "3":
+          newWidth = 50;
+          break;
+        default:
+          console.log("bug in sizeSwitcher");
+    }
+
+    var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
+
+    for (var i = 0; i < randomPizzas.length; i++) {
+      randomPizzas[i].style.width = newWidth + "%";
     }
   }
+
 
   changePizzaSizes(size);
 
